1f34720fd9229618688edfe25c82abe93bfb3375
raaaimund <raimund.rittnauer@gmail.com>
2016-03-06 21:53:16 +0100

    removed update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,6 @@ rm -f /tmp/.X1-lock
 echo "Correcting mount point permissions ..."
 chown "$SINUS_USER":"$SINUS_GROUP" -R "$SINUS_DATA"
 
-echo "Checking for TeamSpeak SinusBot Updates ..."
-sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/sinusbot" -update
-
 echo "Starting TeamSpeak SinusBot ..."
 sudo -u "$SINUS_USER" -g "$SINUS_GROUP" ./"$SINUS_DIR/sinusbot"
 #sudo -u "$SINUS_USER" -g "$SINUS_GROUP" xinit "$SINUS_DIR/ts3bot" -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
